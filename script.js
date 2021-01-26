60b8c043f3d838f1c936fa083ed4e22a78f2ac82
simonas <paulius.simonas@gmail.com>
2021-01-26 13:54:06 +0200

    is kart antra uzduotis

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-const moku=document.querySelector("#moku");
+const moku=document.createElement("div");
+const btn = document.createElement("BUTTON");
+moku.setAttribute("id", "moku");
+moku.innerText="Valio pavyko";
 moku.classList.add("dizainas");
+btn.innerText="spaudaliukas";
+document.body.append(moku);
+document.body.append(btn);
+
+btn.addEventListener("click", function(){
+    this.classList.add("hide");
+    moku.classList.add("hide");
+})
